ccfdab3ba1fa5a56b630a3f9b4d6981f6eda0d42
Ssong-P <hello.sh.day@gmail.com>
2024-01-13 23:28:26 +0900

    Third Commit

---
+++ src/chap04_loop/Break2.java
@@ -0,0 +1,17 @@
+package chap04_loop;
+
+public class For3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+
+        for(;;) {
+            sum = sum + i;
+            if(sum > 10) {
+                System.out.println("i = " + i + ", sum = " + sum);
+                break;
+            }
+            i++;
+        }
+    }
+}
